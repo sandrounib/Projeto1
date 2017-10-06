837a604fa0c951612124d5faafac3dbb22506dce
FIC <sandrounib@hotmail.com>
2017-10-05 21:10:29 -0300

    First Program C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Projeto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 2;
+           Console.WriteLine(a);
+           FazerCafe("caramelo") ;
+           FazerCafe("melao");
+        }
+
+
+        static void FazerCafe(string capsula)
+        {
+            System.Console.WriteLine("Esquentar agua");
+            System.Console.WriteLine("colocar po de magia(cafe)");
+            System.Console.WriteLine("Colocar " + capsula);
+        }
+    }
+}
